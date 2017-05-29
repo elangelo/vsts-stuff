e4253a8e2eee2525f21eea29de9eea8f23724af9
samuel verstraete <samuel.verstraete@gmail.com>
2017-05-29 21:23:45 +0200

    more configuring

--- app/index.ts
+++ app/index.ts
@@ -4,3 +4,16 @@ import * as querystring from "querystring";
 import * as url from "url";
 import * as http from "http";
 import * as https from "https";
+
+/**
+ * config file (config.user.json) looks like:
+ * {
+ *   "url": "https://test.visualstudio.com" ,
+ *   "username": "testuser",
+ *   "token": "testtoken"
+ * }
+ */
+
+import * as config from "./config.user.json";
+console.log ((<any>config).username);
+// var config = require ('./config.user.json');
